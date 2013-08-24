d2b97223fa0e099d5dbec409c3dfb58e24d5ec12
Tomasz Święcicki <tomislater@gmail.com>
2013-08-24 16:13:02 +0200

    Add pytest

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 from random_words import __version__
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 
 setup(
     name='RandomWords',
     version=__version__,
     author='Tomek Święcicki',
     author_email='tomislater@gmail.com',
-    packages=['random_words', 'test'],
-    package_data={'': ['*.txt', '*.dat']},
+    packages=['random_words'],
     url='https://github.com/tomislater/RandomWords',
     license='LICENSE.txt',
     description='A useful module for a random text, e-mails and lorem ipsum.',
@@ -22,6 +37,12 @@ setup(
         'Natural Language :: English',
         'Development Status :: 5 - Production/Stable',
     ],
+    include_package_data=True,
     install_requires=['ujson'],
-    test_suite="pytest",
+    tests_require=['pytest', 'pytest-cov'],
+    cmdclass={'test': PyTest},
+    test_suite='random_words.test.test_random_words',
+    extras_require={
+        'testing': ['pytest', 'pytest-cov'],
+    },
 )

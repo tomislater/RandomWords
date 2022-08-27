2392f875a33da2c224c2438f0602d0a4087d060c
Tomek Święcicki <tomislater@gmail.com>
2022-08-27 12:01:04 +0200

    remove support for 3.10

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     include_package_data=True,

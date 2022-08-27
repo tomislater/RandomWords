e169b2ee0b7fb08092803d19f4c0ae5d51d8d90b
Tomek Święcicki <tomislater@gmail.com>
2022-08-27 12:47:06 +0200

    boring

--- random_words/__init__.py
+++ random_words/__init__.py
@@ -7,7 +7,7 @@
 .. moduleauthor:: Tomek Święcicki <tomislater@gmail.com>
 """
 
-__version__ = '0.2.1'
+__version__ = '0.4.0'
 
 from .random_words import RandomWords
 from .random_words import RandomNicknames
